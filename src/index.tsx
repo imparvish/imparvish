e572c0a33099153e107a96cd25bb4bda150c4008
Anikesh Haran <anikeshharan@Anikeshs-MacBook-Pro.local>
2025-03-22 10:37:47 +0530

    fix index.tsx added css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Use createRoot from React 18
 import App from "./App"; // Ensure correct import
+import './output.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
